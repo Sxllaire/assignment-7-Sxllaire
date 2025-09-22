59e9cd5a2d61ec375a1ffa8f733783a074d8a513
Sxllaire <space.shundere@gmail.com>
2025-09-22 13:49:45 -0500

    Update utilsarray.h

--- utilsarray.h
+++ utilsarray.h
@@ -1,3 +1,13 @@
+//*************************************************************
+//
+//Danil Alieinikov
+//H00880905
+//CSC122-01
+//
+//I certify that this is my work and where appropriate an extension
+//of the starter code provided by the assignment
+//*************************************************************
+
 #ifndef ARRAYUTILS_H
 #define ARRAYUTILS_H
 
