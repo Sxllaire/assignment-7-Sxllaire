7c3daffe12b4aba52ae42a706d6e0c890ae0fd37
Sxllaire <space.shundere@gmail.com>
2025-09-22 13:40:21 -0500

    Create utilsarray.cpp

---
+++ utilsarray.cpp
@@ -0,0 +1,18 @@
+#include "utilsarray.h"
+
+// Sum values in an array
+int sumArray(const int arr[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+// Allocate array and fill it with numbers
+void createArray(int** arr, int size) {
+    *arr = new int[size]; // allocate memory
+    for (int i = 0; i < size; i++) {
+        (*arr)[i] = (i + 1) * 10; // fill with values like 10, 20, 30, ...
+    }
+}
