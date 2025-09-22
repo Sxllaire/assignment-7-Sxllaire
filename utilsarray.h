332cfda56f367d1d0df85aa00af6984a65ee702f
Sxllaire <space.shundere@gmail.com>
2025-09-22 13:39:09 -0500

    Create utilsarray.h

---
+++ utilsarray.h
@@ -0,0 +1,10 @@
+#ifndef ARRAYUTILS_H
+#define ARRAYUTILS_H
+
+// Function to sum values
+int sumArray(const int arr[], int size);
+
+// Function to allocate an array
+void createArray(int** arr, int size);
+
+#endif
